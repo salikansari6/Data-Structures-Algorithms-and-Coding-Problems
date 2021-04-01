755f91ebfda811984d250fa81b3940db00fc79f8
Salik Ansari <salik.ansari6@gmail.com>
2021-04-01 23:23:46 +0530

    Factorial using recursion

--- Recursion.py
+++ Recursion.py
@@ -1,13 +1,30 @@
 class Recursion:
     @staticmethod
     def count_down(num):
-        if num <=0:
+        if num <= 0:
             print('done')
             return
         print(num)
         num -= 1
         Recursion.count_down(num)
 
+    @staticmethod
+    def sum_range(num):
+        if num == 1:
+            return 1
+        return num + Recursion.sum_range(num-1)
+
+    @staticmethod
+    def factorial(num):
+        if num == 0:
+            return 0
+        if num == 1 or num == 2:
+            return num
+        return num * Recursion.factorial(num-1)
+
+
 
 if __name__ == '__main__':
-    Recursion.count_down(4)
+    # Recursion.count_down(4)
+    # print(Recursion.sum_range(4))
+    print(Recursion.factorial(3))
